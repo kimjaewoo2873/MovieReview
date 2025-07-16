a9996d4384d876db57c29c03093ddd3311905133
Kim Jaewoo <jaewoo1427@naver.com>
2025-07-16 20:16:26 +0900

    first dat

--- src/main/java/com/example/MovieReview/controller/pageController.java
+++ src/main/java/com/example/MovieReview/controller/pageController.java
@@ -2,11 +2,18 @@ package com.example.MovieReview.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class pageController {
-    @GetMapping("/home/login")
-    public String login() {
+    @GetMapping("/home/login") // 로그인 홈 화면
+    public String loginHome() {
         return "home/loginpage";
+    }
+    @PostMapping("/login") // 로그인 후, 리스트
+    public String index(@RequestParam String id, @RequestParam String passwd) {
+        return "home/list";
     }
 }
